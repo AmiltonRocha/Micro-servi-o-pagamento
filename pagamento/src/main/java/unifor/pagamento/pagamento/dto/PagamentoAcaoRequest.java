1d5bc91dd20389e0a8042d6d83a580b12b2e410c
Amilton Rocha Holanda <137942476+AmiltonRocha@users.noreply.github.com>
2025-06-06 15:42:16 -0300

    Adicionei o railway

--- pagamento/src/main/java/unifor/pagamento/pagamento/dto/PagamentoAcaoRequest.java
+++ pagamento/src/main/java/unifor/pagamento/pagamento/dto/PagamentoAcaoRequest.java
@@ -1,5 +1,5 @@
 package unifor.pagamento.pagamento.dto;
 
 public class PagamentoAcaoRequest {
-    // Pode ser expandido futuramente
+    
 } 
--- pagamento/src/main/java/unifor/pagamento/pagamento/repository/PagamentoRepository.java
+++ pagamento/src/main/java/unifor/pagamento/pagamento/repository/PagamentoRepository.java
@@ -1,7 +1,8 @@
 package unifor.pagamento.pagamento.repository;
-
+// esse e o CRUD para o banco de dados
+// JpaRepository é uma interface que fornece métodos para operações CRUD básicas
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository; // Anotação para indicar que a classe é um repositório
 import unifor.pagamento.pagamento.model.Pagamento;
 import unifor.pagamento.pagamento.model.StatusPagamento;
 import java.util.List;
